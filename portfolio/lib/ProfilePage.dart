7d8bd9f8c38c4d73dba6fc280849b0dc8bcfe07e
ipshitag <iuippi3@gmail.com>
2019-08-07 01:40:24 +0530

    update

--- portfolio/lib/ProfilePage.dart
+++ portfolio/lib/ProfilePage.dart
@@ -1,11 +1,41 @@
 import 'package:flutter_web/material.dart';
+import 'package:portfolio/responsive_widget.dart';
 
 class ProfilePage  extends StatelessWidget 
 {
+  const ProfilePage({Key key}) : super (key:key);
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+  Widget build(BuildContext context) 
+  {
+        return ResponsiveWidget
+        (
+         largeScreen: Scaffold(
+           backgroundColor: Colors.black,
+           appBar: AppBar(
+             elevation: 0.0,
+             backgroundColor: Colors.black,
+             ) ,
+    
+             drawer: ResponsiveWidget.isSmallScreen(context)
+             ? Drawer(
+               child: ListView(
+                 padding: const EdgeInsets.all(20),
+                 children: <Widget>[],
+                 ),
+                 )
+    
+             : null,
+    
+             body:  SingleChildScrollView(
+               child: AnimatedPadding(
+                 duration: Duration(seconds: 1),
+                 padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+             ),
+         ),
+         
+         
+         ), 
+
     );
   }
 }
