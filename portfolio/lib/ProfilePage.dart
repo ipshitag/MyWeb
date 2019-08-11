6aeba41e41a16c436925eabe04a3860c79639301
ipshitag <iuippi3@gmail.com>
2019-08-11 20:12:17 +0530

    update lib

--- portfolio/lib/ProfilePage.dart
+++ portfolio/lib/ProfilePage.dart
@@ -17,34 +17,43 @@ class ProfilePage  extends StatelessWidget
              ) ,
     
              drawer: ResponsiveWidget.isSmallScreen(context)
-             ? Drawer(
-               child: ListView(
+             ? Drawer
+             (
+               child: ListView
+                 (
                  padding: const EdgeInsets.all(20),
                  children: <Widget>[],
                  ),
-                 )
+             )
     
              : null,
     
-             body:  SingleChildScrollView(
-               child: AnimatedPadding(
+             body:  SingleChildScrollView
+             (
+               child: AnimatedPadding
+               (
                  duration: Duration(seconds: 1),
                  padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
-                 child: ResponsiveWidget(
-                   largeScreen: Column(
-                   mainAxisAlignment: MainAxisAlignment.start,
-                   children: <Widget>[
-                   NavHeader(),
-                   SizedBox
+                 child: ResponsiveWidget
+                 (
+                   largeScreen: Column
                    (
-                     height: MediaQuery.of(context).size.height * 0.1,
-                   ),
-                  ProfileInfo(),
-                  SizedBox
-                  (
-                    height: MediaQuery.of(context).size.height * 0.2,
-                  ),
-                  SocialInfo(),
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   children: <Widget>
+                   [
+                    NavHeader(),
+                    SizedBox
+                      (
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                  
+                    ProfileInfo(),
+                    SizedBox
+                      (
+                        height: MediaQuery.of(context).size.height * 0.2,
+                      ),
+                      
+                    SocialInfo(),
                    ],
                    ),
                  )
@@ -54,6 +63,32 @@ class ProfilePage  extends StatelessWidget
          
          ), 
 
+    );
+  }
+}
+
+
+
+
+class NavHeader extends StatelessWidget 
+{
+  final List<Widget> navButtons;
+  const NavHeader({Key key, this.navButtons}) : super(key: key);
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          PKDot(),//Spacer(),
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: navButtons,
+            )
+        ],
+      ),
     );
   }
 }
